b73ede6beb73266a0052ed5618efd2b759040ff2
Clev123 <clevenchin08@gmail.com>
2026-07-15 12:11:38 +0800

    labs/lab03/exercise8/lab-03-8.fprg labs/lab03/exercise8/lab-03-8.py

--- labs/lab03/exercise8/lab-03-8.py
+++ labs/lab03/exercise8/lab-03-8.py
@@ -1,9 +1,10 @@
 principal = float(input())
 rate = float(input())
 time = float(input())
-interest = principal * rate * time
+interest = principal * rate / 100 * time
 print(interest)
-totalAmount = principal + principal * 1 * rate * time
+totalAmount = principal + principal * 1 * rate / 100 * time
 print(totalAmount)
 monthlyInterest = interest / time * 12
 print(monthlyInterest)
+
