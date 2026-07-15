d1225366e9a35abbebe5dc35fe526ea1952f01a0
Clev123 <clevenchin08@gmail.com>
2026-07-15 12:18:10 +0800

    labs/lab03/exercise8/lab-03-8.fprg labs/lab03/exercise8/lab-03-8.py

--- labs/lab03/exercise8/lab-03-8.py
+++ labs/lab03/exercise8/lab-03-8.py
@@ -5,6 +5,6 @@ interest = principal * rate / 100 * time
 print(interest)
 totalAmount = principal + principal * 1 * rate / 100 * time
 print(totalAmount)
-monthlyInterest = interest / time * 12
+monthlyInterest = interest * time / 12
 print(monthlyInterest)
 
