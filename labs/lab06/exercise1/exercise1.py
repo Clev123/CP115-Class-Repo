d0b27de4a87aec0b8eded926e24a5e4cd15e9f62
Clev123 <clevenchin08@gmail.com>
2026-09-02 10:59:19 +0800

    labs/lab06/exercise1/exercise1.py

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -3,19 +3,19 @@
 # Calculate every total, subtotal, and tax in your code. Do not type the money
 # amounts in directly. Show every amount with exactly two decimal places.
 # Creating a formatted table
-coffee_name = "Coffee"
+coffee_name = "Coffee" # Assign Coffee name and values
 coffee_price = 3.50
 coffee_quantity = 2
 
-muffin_name = "Muffin"
+muffin_name = "Muffin" # Assign Muffin name and values
 muffin_price = 2.10
 muffin_quantity = 3
 
-water_name = "Water"
+water_name = "Water" # Assign Water name and values
 water_price = 1.05
 water_quantity = 4
 
-coffee_total = coffee_price * coffee_quantity
+coffee_total = coffee_price * coffee_quantity # Calculate total for Coffee, Muffin and Water
 muffin_total = muffin_price * muffin_quantity
 water_total = water_price * water_quantity
 
